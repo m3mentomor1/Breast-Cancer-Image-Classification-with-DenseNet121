e1e67058aebcb8c8e45edc54e8adde8f63b4e73c
Shain Sahagun <95956735+m3mentomor1@users.noreply.github.com>
2023-12-08 22:59:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,13 +6,6 @@ from PIL import Image
 import requests
 import h5py
 
-# Class mapping
-class_mapping = {
-    0: 'Benign',
-    1: 'Malignant',
-    2: 'Normal',
-}
-
 # Function to load the combined model
 @st.cache(allow_output_mutation=True)
 def load_model():
@@ -30,7 +23,7 @@ def load_model():
     with h5py.File(BytesIO(model_bytes), 'r') as hf:
         # Load the combined model
         model = tf.keras.models.load_model(hf)
-
+    
     return model
 
 # Function to preprocess and make predictions
@@ -43,10 +36,7 @@ def predict(image, model):
 
     # Make prediction
     predictions = model.predict(img_array)
-
-    # Get the predicted class
-    predicted_class = class_mapping[np.argmax(predictions)]
-    return predicted_class
+    return predictions
 
 # Streamlit app
 st.title('Breast Cancer Image Classification')
@@ -61,5 +51,5 @@ if uploaded_file is not None:
     model = load_model()
 
     # Make predictions
-    predicted_class = predict(image, model)
-    st.write(f"Prediction: {predicted_class}")
+    predictions = predict(image, model)
+    st.write(f"Predictions: {predictions}")
